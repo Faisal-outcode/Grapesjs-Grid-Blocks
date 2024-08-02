f488a860ce3e6228e8209b015b5004afe3a62f1c
faisalakram <faisalakram1133@gmial.com>
2024-08-02 18:43:04 +0500

    options changes for npm

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-import grapesjs from 'grapesjs';
+import grapesjs from "grapesjs";
 
-export default grapesjs.plugins.add('gjs-blocks-basic', (editor, opts = {}) => {
+export default grapesjs.plugins.add("gjs-blocks-basic", (editor, opts = {}) => {
   const config = {
-    blocks: ['column1', 'column2', 'column3', 'column3-7', 'text', 'link', 'image', 'video', 'map'],
+    blocks: ["column1", "column2", "column3", "column3-7", "column7-3"],
     flexGrid: 0,
-    stylePrefix: 'gjs-',
+    stylePrefix: "gjs-",
     addBasicStyle: true,
-    category: 'Basic',
-    labelColumn1: '1 Column',
-    labelColumn2: '2 Columns',
-    labelColumn3: '3 Columns',
-    labelColumn37: '2 Columns 3/7',
-    labelText: 'Text',
-    labelLink: 'Link',
-    labelImage: 'Image',
-    labelVideo: 'Video',
-    labelMap: 'Map',
-    ...opts
+    category: "Basic",
+    labelColumn1: "1 Column",
+    labelColumn2: "2 Columns",
+    labelColumn3: "3 Columns",
+    labelColumn4: "4 Columns",
+    labelColumn5: "5 Columns",
+    labelColumn6: "6 Columns",
+    labelColumn37: "2 Columns 3/7",
+    labelColumn73: "2 Columns 7/3",
+    ...opts,
   };
 
   // Add blocks
-  const loadBlocks = require('./blocks');
+  const loadBlocks = require("./blocks");
   loadBlocks.default(editor, config);
-
 });
